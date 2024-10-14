0bc8d656af480aa423f4c92db4fa67b32486845a
gulshensettarova <gulshensettarova@gmail.com>
2024-10-14 22:10:27 +0400

    Initial Project & Added MessagingConfig class

---
+++ rabbitmq-demo/src/main/java/com/company/rabbitmq_demo/config/MessagingConfig.java
@@ -0,0 +1,44 @@
+package com.company.rabbitmq_demo.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessagingConfig {
+
+    private static final String  MAIL_QUEUE = "email_notifications_queue";
+    private static final String MAIL_EXCHANGE = "email_exchange";
+    private static final String MAIL_OUTING_KEY = "email_routingKey";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(MAIL_QUEUE);
+    }
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(MAIL_EXCHANGE);
+    }
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(MAIL_OUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+
+}
